a1a5b7d7f39ccb79b1dc8d483aac4952ba399e51
Eugene Dubovik <dev.dubovik@gmail.com>
2022-06-17 13:35:51 +0300

    change returned type getCourseService()

--- src/main/java/com/epam/optionalcourse/service/factory/ServiceFactory.java
+++ src/main/java/com/epam/optionalcourse/service/factory/ServiceFactory.java
@@ -1,13 +1,16 @@
 package com.epam.optionalcourse.service.factory;
 
+import com.epam.optionalcourse.service.CourseService;
 import com.epam.optionalcourse.service.UserService;
+import com.epam.optionalcourse.service.impl.CourseServiceImpl;
 import com.epam.optionalcourse.service.impl.UserServiceImpl;
 
 public class ServiceFactory {
 
     private static final ServiceFactory INSTANCE = new ServiceFactory();
 
-    private final UserServiceImpl userService = UserServiceImpl.getInstance();
+    private final UserServiceImpl userService = new UserServiceImpl();
+    private final CourseServiceImpl courseService = new CourseServiceImpl();
 
 
     private ServiceFactory() {
@@ -19,5 +22,9 @@ public class ServiceFactory {
 
     public UserService getUserService(){
         return userService;
+    }
+
+    public CourseService getCourseService() {
+        return courseService;
     }
 }
