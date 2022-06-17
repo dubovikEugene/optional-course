a890693f2a4327db2747302c18d1a5969efb9977
Eugene Dubovik <dev.dubovik@gmail.com>
2022-06-17 13:19:36 +0300

    add implementation

--- src/main/java/com/epam/optionalcourse/controller/command/impl/Registration.java
+++ src/main/java/com/epam/optionalcourse/controller/command/impl/Registration.java
@@ -1,13 +1,44 @@
 package com.epam.optionalcourse.controller.command.impl;
 
+import com.epam.optionalcourse.bean.CreateUser;
 import com.epam.optionalcourse.controller.command.Command;
+import com.epam.optionalcourse.service.exception.ServiceException;
+import com.epam.optionalcourse.service.factory.ServiceFactory;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class Registration implements Command {
+    private final ServiceFactory serviceFactory = ServiceFactory.getInstance();
+
+    private static final String REQUEST_PARAMETER_FIRST_NAME = "firstName";
+    private static final String REQUEST_PARAMETER_LAST_NAME = "lastName";
+    private static final String REQUEST_PARAMETER_BIRTHDAY = "birthday";
+    private static final String REQUEST_PARAMETER_EMAIL = "email";
+    private static final String REQUEST_PARAMETER_PASSWORD = "password";
+    private static final String REQUEST_PARAMETER_GENDER = "gender";
+
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
+        var createUser = buildCreateUser(request);
+        try {
+            var authorizedUser = serviceFactory.getUserService().register(createUser);
+            request.setAttribute("user", authorizedUser);
+            // TODO: 6/10/2022 logic
+        } catch (ServiceException e) {
+            throw new RuntimeException(e);
+        }
 
     }
+
+    private CreateUser buildCreateUser(HttpServletRequest request) {
+        return new CreateUser(
+                request.getParameter(REQUEST_PARAMETER_FIRST_NAME),
+                request.getParameter(REQUEST_PARAMETER_LAST_NAME),
+                request.getParameter(REQUEST_PARAMETER_BIRTHDAY),
+                request.getParameter(REQUEST_PARAMETER_EMAIL),
+                request.getParameter(REQUEST_PARAMETER_PASSWORD),
+                request.getParameter(REQUEST_PARAMETER_GENDER));
+    }
+
 }
--- src/main/java/com/epam/optionalcourse/controller/command/impl/SignIn.java
+++ src/main/java/com/epam/optionalcourse/controller/command/impl/SignIn.java
@@ -1,13 +1,42 @@
 package com.epam.optionalcourse.controller.command.impl;
 
+import com.epam.optionalcourse.bean.AuthorizedUser;
 import com.epam.optionalcourse.controller.command.Command;
+import com.epam.optionalcourse.service.exception.ServiceException;
+import com.epam.optionalcourse.service.factory.ServiceFactory;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 public class SignIn implements Command {
 
-    @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) {
+    private static final String REQUEST_EMAIL = "email";
+    private static final String REQUEST_PASSWORD = "password";
+    private static final String USER_SESSION_ATTRIBUTE = "user";
+    private static final String SUCCESS_REDIRECT_COMMAND = "controller?command=get_main_page";
+    private static final String FAILURE_REDIRECT_COMMAND = "controller?command=get_sign_in_page&error=sign_in_error";
+    ServiceFactory serviceFactory = ServiceFactory.getInstance();
 
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            var authorizedUser = serviceFactory.getUserService()
+                    .signIn(request.getParameter(REQUEST_EMAIL), request.getParameter(REQUEST_PASSWORD));
+            onSuccessLogin(authorizedUser, request, response);
+        } catch (ServiceException e) {
+            // TODO: 6/13/2022 logger
+            onFailureLogin(request, response);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void onFailureLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.sendRedirect(FAILURE_REDIRECT_COMMAND);
+    }
+
+    private void onSuccessLogin(AuthorizedUser authorizedUser, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().setAttribute(USER_SESSION_ATTRIBUTE, authorizedUser);
+        response.sendRedirect(SUCCESS_REDIRECT_COMMAND);
     }
 }
