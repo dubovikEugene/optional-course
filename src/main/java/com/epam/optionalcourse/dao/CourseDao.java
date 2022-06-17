9543be090c66f529299eee1d31307929ce1161f5
Eugene Dubovik <dev.dubovik@gmail.com>
2022-06-17 12:49:54 +0300

    add new method

--- src/main/java/com/epam/optionalcourse/dao/CourseDao.java
+++ src/main/java/com/epam/optionalcourse/dao/CourseDao.java
@@ -1,9 +1,14 @@
 package com.epam.optionalcourse.dao;
 
 import com.epam.optionalcourse.bean.Course;
+import com.epam.optionalcourse.bean.ReadCourse;
+import com.epam.optionalcourse.dao.exception.DaoException;
+
+import java.util.List;
 
 public interface CourseDao {
 
+    List<ReadCourse> getAllCourses() throws DaoException;
     void createCourse(Course course);
     void updateCourse(Course course);
 
