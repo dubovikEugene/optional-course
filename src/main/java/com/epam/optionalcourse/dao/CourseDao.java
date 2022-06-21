ec89316fab1824839cb5cabcb07dd3c5678e961a
Eugene Dubovik <dev.dubovik@gmail.com>
2022-06-21 14:55:20 +0300

    add findById

--- src/main/java/com/epam/optionalcourse/dao/CourseDao.java
+++ src/main/java/com/epam/optionalcourse/dao/CourseDao.java
@@ -1,15 +1,17 @@
 package com.epam.optionalcourse.dao;
 
 import com.epam.optionalcourse.bean.Course;
-import com.epam.optionalcourse.bean.ReadCourse;
+import com.epam.optionalcourse.bean.ReadCourseRun;
 import com.epam.optionalcourse.dao.exception.DaoException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CourseDao {
 
-    List<ReadCourse> getAllCourses() throws DaoException;
+    List<ReadCourseRun> getAllCourses() throws DaoException;
     void createCourse(Course course);
     void updateCourse(Course course);
 
+    Optional<ReadCourseRun> findById(Integer courseId) throws DaoException;
 }
--- src/main/java/com/epam/optionalcourse/dao/impl/CourseDaoImpl.java
+++ src/main/java/com/epam/optionalcourse/dao/impl/CourseDaoImpl.java
@@ -1,7 +1,7 @@
 package com.epam.optionalcourse.dao.impl;
 
 import com.epam.optionalcourse.bean.Course;
-import com.epam.optionalcourse.bean.ReadCourse;
+import com.epam.optionalcourse.bean.ReadCourseRun;
 import com.epam.optionalcourse.dao.CourseDao;
 import com.epam.optionalcourse.dao.connectionpool.ConnectionPool;
 import com.epam.optionalcourse.dao.exception.ConnectionPoolException;
@@ -11,26 +11,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CourseDaoImpl implements CourseDao {
     private static final ConnectionPool pool = ConnectionPool.getInstance();
 
     private static final String FIND_ALL_SQL = """
-            SELECT c.id, c.course_name, c.start_date, c.end_date, c.description, u.first_name, u.last_name
-            FROM courses AS c JOIN users u on u.id = c.teacher_id
+            SELECT cr.id, c.course_name, cr.start_date, cr.end_date, c.description, cr.course_program, u.first_name, u.last_name
+            FROM courses_runs AS cr
+                JOIN courses c
+            on cr.courses_id = c.id
+                JOIN users u
+            on u.id = cr.teacher_id
             WHERE is_ended IS NULL
-              AND flag IS NULL
+              AND c.flag IS NULL
+            """;
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + """
+             AND cr.id = ?
             """;
 
     public CourseDaoImpl() {
     }
 
     @Override
-    public List<ReadCourse> getAllCourses() throws DaoException {
+    public List<ReadCourseRun> getAllCourses() throws DaoException {
 
         try (var connection = pool.get();
              var preparedStatement = connection.prepareStatement(FIND_ALL_SQL)) {
-            List<ReadCourse> courses = new ArrayList<>();
+            List<ReadCourseRun> courses = new ArrayList<>();
             var resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 courses.add(buildReadCourse(resultSet));
@@ -44,16 +52,23 @@ public class CourseDaoImpl implements CourseDao {
 
     }
 
-    private ReadCourse buildReadCourse(ResultSet resultSet) throws SQLException {
-        return new ReadCourse(
-                resultSet.getInt("id"),
-                resultSet.getString("course_name"),
-                resultSet.getDate("start_date").toLocalDate(),
-                resultSet.getDate("end_date").toLocalDate(),
-                resultSet.getString("description"),
-                resultSet.getString("first_name"),
-                resultSet.getString("last_name")
-        );
+    @Override
+    public Optional<ReadCourseRun> findById(Integer courseId) throws DaoException {
+        try (var connection = pool.get();
+             var preparedStatement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            preparedStatement.setInt(1, courseId);
+
+            var resultSet = preparedStatement.executeQuery();
+            ReadCourseRun readCourse = null;
+            if (resultSet.next()) {
+                readCourse = buildReadCourse(resultSet);
+            }
+
+            return Optional.ofNullable(readCourse);
+
+        } catch (SQLException | ConnectionPoolException e) {
+            throw new DaoException(e);
+        }
     }
 
     @Override
@@ -64,5 +79,18 @@ public class CourseDaoImpl implements CourseDao {
     @Override
     public void updateCourse(Course course) {
 
+    }
+
+    private ReadCourseRun buildReadCourse(ResultSet resultSet) throws SQLException {
+        return new ReadCourseRun(
+                resultSet.getInt("id"),
+                resultSet.getString("course_name"),
+                resultSet.getDate("start_date").toLocalDate(),
+                resultSet.getDate("end_date").toLocalDate(),
+                resultSet.getString("description"),
+                resultSet.getString("course_program"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name")
+        );
     }
 }
