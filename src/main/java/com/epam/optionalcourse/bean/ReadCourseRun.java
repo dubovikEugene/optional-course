310d917f06552310f31cf218d5c99465b08126e8
Eugene Dubovik <dev.dubovik@gmail.com>
2022-06-21 14:57:19 +0300

    change fields

--- src/main/java/com/epam/optionalcourse/bean/ReadCourseRun.java
+++ src/main/java/com/epam/optionalcourse/bean/ReadCourseRun.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class ReadCourse implements Serializable {
+public class ReadCourseRun implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 285880707909758916L;
@@ -14,25 +14,29 @@ public class ReadCourse implements Serializable {
     private LocalDate startDate;
     private LocalDate endDate;
     private String description;
+
+    private String courseProgram;
     private String teacherFirstName;
     private String teacherLastName;
 
 
-    public ReadCourse() {
+    public ReadCourseRun() {
     }
 
-    public ReadCourse(Integer id,
-                      String courseName,
-                      LocalDate startDate,
-                      LocalDate endDate,
-                      String description,
-                      String teacherFirstName,
-                      String teacherLastName) {
+    public ReadCourseRun(Integer id,
+                         String courseName,
+                         LocalDate startDate,
+                         LocalDate endDate,
+                         String description,
+                         String courseProgram,
+                         String teacherFirstName,
+                         String teacherLastName) {
         this.id = id;
         this.courseName = courseName;
         this.startDate = startDate;
         this.endDate = endDate;
         this.description = description;
+        this.courseProgram = courseProgram;
         this.teacherFirstName = teacherFirstName;
         this.teacherLastName = teacherLastName;
     }
@@ -77,6 +81,14 @@ public class ReadCourse implements Serializable {
         this.description = description;
     }
 
+    public String getCourseProgram() {
+        return courseProgram;
+    }
+
+    public void setCourseProgram(String courseProgram) {
+        this.courseProgram = courseProgram;
+    }
+
     public String getTeacherFirstName() {
         return teacherFirstName;
     }
@@ -97,12 +109,13 @@ public class ReadCourse implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        ReadCourse course = (ReadCourse) o;
+        ReadCourseRun course = (ReadCourseRun) o;
         return Objects.equals(id, course.id)
                 && Objects.equals(courseName, course.courseName)
                 && Objects.equals(startDate, course.startDate)
                 && Objects.equals(endDate, course.endDate)
                 && Objects.equals(description, course.description)
+                && Objects.equals(courseProgram, course.courseProgram)
                 && Objects.equals(teacherFirstName, course.teacherFirstName)
                 && Objects.equals(teacherLastName, course.teacherLastName);
     }
@@ -114,6 +127,7 @@ public class ReadCourse implements Serializable {
                 startDate,
                 endDate,
                 description,
+                courseProgram,
                 teacherFirstName,
                 teacherLastName);
     }
